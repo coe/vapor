5530df27ede3eda31918645611e0120e7dc2619a
tsuyoshi hyuga <hyuga@amazia.co.jp>
2019-12-20 10:44:15 +0900

    initial

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,8 +13,7 @@ public func routes(_ router: Router) throws {
     print("hyuu string:\(string)")
     
 
-    var progress = Progress()
-    progress.totalUnitCount = 5_312_764
+    let progress = Progress(totalUnitCount: 5_312_764)
     progress.completedUnitCount = 419_240
     print("hyuu progress localizedDescription:\(String(describing: progress.localizedDescription))")
     print("hyuu progress localizedAdditionalDescription:\(String(describing: progress.localizedAdditionalDescription))")
